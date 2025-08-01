a2a7413b9fd0af5566abd4fff2c79e5236f58274
moronali09 <jonibou04@gmail.com>
2025-08-01 22:18:36 +0600

    Create search.js

---
+++ pages/api/search.js
@@ -0,0 +1,14 @@
+import ytsr from 'ytsr';
+export default async function handler(req, res) {
+  const { q } = req.query;
+  if (!q) return res.status(400).json({ error: 'No query' });
+  const filters = await ytsr.getFilters(q);
+  const filter = filters.get('Type').get('Video');
+  const searchResults = await ytsr(filter.url, { limit: 5 });
+  const items = searchResults.items.map(item => ({
+    id: item.id,
+    title: item.title,
+    url: item.url
+  }));
+  res.status(200).json({ results: items });
+}
