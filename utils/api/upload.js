410fdff1768903eb48426f6900f7b2f7eb56cc6f
moronali09 <jonibou04@gmail.com>
2025-08-02 11:55:48 +0600

    Update upload.js

--- utils/api/upload.js
+++ utils/api/upload.js
@@ -1,12 +1,12 @@
 import { IncomingForm } from 'formidable-serverless';
-import fs from 'fs-extra';
-import { STORAGE } from '../../utils/storage';
+import { uploadObject } from '../../utils/s3';
 export const config = { api: { bodyParser: false } };
 export default async (req, res) => {
-  const form = new IncomingForm({ uploadDir: '/tmp' });
+  const form = new IncomingForm();
   form.parse(req, async (err, fields, files) => {
     const file = files.file;
-    await fs.move(file.filepath, `${STORAGE}/${file.originalFilename}`, { overwrite: true });
+    const data = await fs.promises.readFile(file.filepath);
+    await uploadObject(file.originalFilename, data);
     res.status(200).json({ ok: true });
   });
 };
