a1b887786d9dafd8eb0edc6acbc7e48cd478d647
moronali09 <jonibou04@gmail.com>
2025-08-02 11:50:53 +0600

    Update list.js

--- utils/api/list.js
+++ utils/api/list.js
@@ -1,10 +1,5 @@
-import { promises as fs } from 'fs';
-import { STORAGE } from '../../utils/storage';
+import { listObjects } from '../../utils/s3';
 export default async function handler(req, res) {
-  const names = await fs.readdir(STORAGE);
-  const entries = await Promise.all(names.map(async name => {
-    const stat = await fs.stat(`${STORAGE}/${name}`);
-    return { name, isDirectory: stat.isDirectory() };
-  }));
-  res.status(200).json(entries);
+  const keys = await listObjects();
+  res.status(200).json(keys);
 }
